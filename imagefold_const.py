52a1419fa4691b0e1df63361b836114d7f1f6771
mikeaalv <yue.wu@uga.edu>
2020-02-11 13:05:11 -0500

    Update imagefold_const.py
    created test/train folder for general image processing pipeline

--- imagefold_const.py
+++ imagefold_const.py
@@ -1,4 +1,6 @@
 ##only groups image with single classifications
+##the training and testing sample will be constructed so that each class is with equal number of samples in training and validation group
+## selecting into training+validation and validation group are random
 import argparse
 import os
 import random
@@ -15,6 +17,7 @@ import h5py
 from nltk import flatten
 import re
 import pandas as pd
+from collections import Counter
 
 import torch
 import torch.nn.parallel
@@ -28,6 +31,7 @@ from torch.utils.data.sampler import Sampler
 import torchvision
 from torchvision import datasets, models, transforms
 inputdir="../data/"
+testperc=0.2
 datatab=pd.read_csv(inputdir+"Classifications.csv",delimiter=",")
 classcol=np.array(datatab['CLASS'].tolist())
 rowind=np.isin(classcol,np.array(['WRAP','WT','BULKY']))
@@ -35,15 +39,34 @@ datatabclean=datatab.loc[rowind]
 
 # file_names=datatabclean.loc[:,'IMG']
 classlab=np.unique(datatabclean['CLASS'].tolist())
+counterlist=Counter(datatabclean['CLASS'].tolist())
+totsampsize=min(counterlist.values())# for each class
+numsamptest=math.floor(totsampsize*testperc)
+sampleind=set(range(0,totsampsize))
+testind=np.sort(np.array(random.sample(sampleind,numsamptest)))
+trainind=np.sort(np.array(list(sampleind.difference(set(testind)))))
+
 # load resize image
+os.makedirs(inputdir+'LApops_classify/train',exist_ok=True)
+os.makedirs(inputdir+'LApops_classify/test',exist_ok=True)
+random.seed(1)
 for classele in classlab:
-    os.makedirs(inputdir+'LApops_classify/'+classele,exist_ok=True)
+    os.makedirs(inputdir+'LApops_classify/train/'+classele,exist_ok=True)
+    os.makedirs(inputdir+'LApops_classify/test/'+classele,exist_ok=True)
     classcol=np.array(datatabclean['CLASS'].tolist())
     rowind=np.isin(classcol,classele)
     datatabsub=datatabclean.loc[rowind]
-    files=datatabsub['IMG'].tolist()
-    for file in files:
+    totsamp_ind=np.array(random.sample(list(range(0,datatabsub.shape[0])),totsampsize))
+    datatabsub2=datatabsub.iloc[totsamp_ind]
+    files=np.array(datatabsub2['IMG'].tolist())
+    for file in files[trainind]:
         file=file+'.tif'
         sourcfile=inputdir+"LApops_expand/"+file
         if os.path.isfile(sourcfile):
-            shutil.copy(sourcfile,inputdir+'LApops_classify/'+classele+"/"+file)
+            shutil.copy(sourcfile,inputdir+'LApops_classify/train/'+classele+"/"+file)
+    
+    for file in files[testind]:
+        file=file+'.tif'
+        sourcfile=inputdir+"LApops_expand/"+file
+        if os.path.isfile(sourcfile):
+            shutil.copy(sourcfile,inputdir+'LApops_classify/test/'+classele+"/"+file)
