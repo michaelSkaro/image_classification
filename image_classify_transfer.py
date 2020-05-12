d5d2c5508613394e9644b64d210f11dfd9d6fbcc
mikeaalv <yue.wu@uga.edu>
2020-05-12 11:03:27 -0400

    Update image_classify_transfer.py
    small format update

--- image_classify_transfer.py
+++ image_classify_transfer.py
@@ -157,11 +157,11 @@ def save_checkpoint(state,is_best,is_best_train,filename='checkpoint.resnetode.t
 def imshow(inp, title=None):
     """Imshow for Tensor."""
     #from https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
+    inp=inp.numpy().transpose((1,2,0))
+    mean=np.array([0.485,0.456,0.406])
+    std=np.array([0.229,0.224,0.225])
+    inp=std*inp+mean
+    inp=np.clip(inp,0,1)
     plt.imshow(inp)
     if title is not None:
         plt.title(title)
@@ -169,27 +169,27 @@ def imshow(inp, title=None):
 
 def visualize_model(model,dataloaders,num_images=6):
     #from https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
-    was_training = model.training
+    was_training=model.training
     model.eval()
-    images_so_far = 0
-    fig = plt.figure()
+    images_so_far=0
+    fig=plt.figure()
 
     with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+        for i, (inputs,labels) in enumerate(dataloaders):
+            inputs=inputs.to(device)
+            labels=labels.to(device)
 
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
+            outputs=model(inputs)
+            _, preds=torch.max(outputs,1)
 
             for j in range(inputs.size()[0]):
                 images_so_far += 1
-                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax=plt.subplot(num_images//2,2,images_so_far)
                 ax.axis('off')
                 ax.set_title('predicted: {}, labels: {}'.format(class_names[preds[j]],class_names[labels[j]]))
                 imshow(inputs.cpu().data[j])
 
-                if images_so_far == num_images:
+                if images_so_far==num_images:
                     model.train(mode=was_training)
                     return
         model.train(mode=was_training)
