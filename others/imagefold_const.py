d686a45097384ed493c0aa90a889ce5b2eddb23c
mikeaalv <yue.wu@uga.edu>
2020-08-11 16:42:08 -0400

    Update imagefold_const.py
    correct reampling error

--- others/imagefold_const.py
+++ others/imagefold_const.py
@@ -1,6 +1,6 @@
 ##only groups image with single classifications
-##the training and testing sample will be constructed so that each class is with equal number of samples in training and validation group
-## selecting into training+validation and validation group are random
+## samples are separated so that each class is with equal number of samples in training, validation, and testing group
+## separation are random
 import argparse
 import os
 import random
@@ -36,8 +36,10 @@ inputdir="/Users/yuewu/Dropbox (Edison_Lab@UGA)/Projects/Bioinformatics_modeling
 testperc=[0.1,0.1]#valid and test
 datatab=pd.read_csv(inputdir+"Classifications.csv",delimiter=",")
 classcol=np.array(datatab['CLASS'].tolist())
+#match single class
 rowind=np.isin(classcol,np.array(['WRAP','WT','BULKY']))
 datatabclean=datatab.loc[rowind]
+#no repeats
 nonduplicated_ind=[not ele for ele in datatabclean['IMG'].duplicated(keep=False).tolist()]
 datatabclean2=datatabclean.loc[nonduplicated_ind]
 
@@ -49,20 +51,23 @@ numsampvalid=math.floor(totsampsize*testperc[0])
 numsamptest=math.floor(totsampsize*testperc[1])
 sampleind=set(range(0,totsampsize))
 testind=np.sort(np.array(random.sample(sampleind,numsamptest)))
-validind=np.sort(np.array(random.sample(sampleind,numsampvalid)))
 testindset=set(testind)
+validind=np.sort(np.array(random.sample(sampleind.difference(testindset),numsampvalid)))
 validindset=set(validind)
 trainind=np.sort(np.array(list(sampleind.difference(testindset.union(validindset)))))
 
 os.makedirs(inputdir+'LApops_classify',exist_ok=True)
 os.makedirs(inputdir+'LApops_expand',exist_ok=True)
+os.makedirs(inputdir+'LApops_classify_singleclass',exist_ok=True)
+os.makedirs(inputdir+'LApops_classify_intepret',exist_ok=True)
 os.makedirs(inputdir+'LApops_classify/train',exist_ok=True)
 os.makedirs(inputdir+'LApops_classify/test',exist_ok=True)
 os.makedirs(inputdir+'LApops_classify/validate',exist_ok=True)
-for direle in os.listdir(inputdir+'LApops_Raw'):
-    if os.path.isdir(inputdir+'LApops_Raw/'+direle):
-        for fileele in os.listdir(inputdir+'LApops_Raw/'+direle):
-            sourcfile=inputdir+'LApops_Raw/'+direle+'/'+fileele
+for direle in os.listdir(inputdir+'LApops_Raw_clean'):
+    dir_path=inputdir+'LApops_Raw_clean/'+direle
+    if os.path.isdir(dir_path):
+        for fileele in os.listdir(dir_path):
+            sourcfile=dir_path+'/'+fileele
             targetfile=inputdir+'LApops_expand/'+fileele
             shutil.copy(sourcfile,targetfile)
 
@@ -101,12 +106,46 @@ for classele in classlab:
         else:
             print('non existence file:'+file+'\n')
 
+# single class test
+os.makedirs(inputdir+'LApops_classify_singleclass/test/',exist_ok=True)
+for classele in classlab:
+    os.makedirs(inputdir+'LApops_classify_singleclass/test/'+classele,exist_ok=True)
+    for classele2 in classlab:
+        os.makedirs(inputdir+'LApops_classify_singleclass/test/'+classele+'/'+classele2,exist_ok=True)
+    sourcedir=inputdir+'LApops_classify/test/'+classele+"/"
+    for file in os.listdir(sourcedir):
+        sourcfile=sourcedir+file
+        targetfile=inputdir+'LApops_classify_singleclass/test/'+classele+'/'+classele+'/'+file
+        if os.path.isfile(sourcfile):
+            shutil.copy(sourcfile,targetfile)
+        else:
+            print('non existence file:'+file+'\n')
+
+# random selected intepretation image from test set
+sampsize=10
+os.makedirs(inputdir+'LApops_classify_intepret/test/',exist_ok=True)
+for classele in classlab:
+    os.makedirs(inputdir+'LApops_classify_intepret/test/'+classele,exist_ok=True)
+    sourcedir=inputdir+'LApops_classify/test/'+classele+"/"
+    testfiles=os.listdir(sourcedir)
+    sele_files=random.sample(testfiles,sampsize)
+    for file in sele_files:
+        sourcfile=sourcedir+file
+        targetfile=inputdir+'LApops_classify_intepret/test/'+classele+'/'+file
+        if os.path.isfile(sourcfile):
+            shutil.copy(sourcfile,targetfile)
+        else:
+            print('non existence file:'+file+'\n')
+
 # For external test data set
-inputdir="/Users/yuewu/Dropbox (Edison_Lab@UGA)/Projects/Bioinformatics_modeling/emi_nnt_image/data/LApops_new_Raw/"
-outputdir="/Users/yuewu/Dropbox (Edison_Lab@UGA)/Projects/Bioinformatics_modeling/emi_nnt_image/data/LApops_new_test/test/"
+sourcedir=inputdir+"LApops_new_Raw/"
+outputdir=inputdir+"LApops_new_test/"
+os.makedirs(outputdir,exist_ok=True)
+rundir=outputdir+'test'
+os.makedirs(rundir,exist_ok=True)
 classtypes=np.array(['WRAP','WT','BULKY'])
 for classtype in classtypes:
-    os.makedirs(outputdir+classtype,exist_ok=True)
+    os.makedirs(rundir+classtype,exist_ok=True)
 
 infortab_list=[]
 for batchdir in os.listdir(inputdir):
